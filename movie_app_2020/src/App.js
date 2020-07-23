ba42942e59c922ffdd7d25c8eec3592bc2e61f8e
kmlee95 <kmlee95@naver.com>
2020-07-23 09:08:07 +0900

    fix : axios add

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
+import axios from "axios";
 
-function Food(props) {
-  //console.log({fav});
-  return (
-    <div>
-      <h1>I like {props.name}</h1> <h2>i data {props.picture}</h2>
-    </div>
-  );
-}
-const foodILike = [
-  { id: 1, name: "kimchi", data: "hi" },
-  { id: 2, name: "pizza", data: "kiki" },
-];
-
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.data} />;
-}
-
-function App() {
-  return <div>{foodILike.map(renderFood)}</div>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json"); //axios로 나온 값의 data의 data의 movies를 찾아서 넣어줌
+    this.setState({ movies, isLoading: false }); //movies:movies 다 했을 때 loading을 false
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+  }
 }
 
 export default App;
